e52801ba5eaea44ba7a35a6029f75a0ad26f8c08
LaraKittens <Ever.maintenant.pro@gmail.com>
2022-09-29 12:17:35 +0200

    init the index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello world!'));
+
+console.log(chalk.red.bgYellow("Xavier"));
+console.log(chalk.green.bgYellow("Antonin"));
+console.log(chalk.blue.bgYellow("Guillaume"));
+console.log(chalk.magenta.bgYellow("Joryan"));
